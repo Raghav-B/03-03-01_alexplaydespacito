d29de37e9dbd9193adab56b191b5b30ebb87d9d1
nwjbrandon <nwjbrandon@gmail.com>
2019-03-28 23:27:58 +0800

    dynamic reconfigure in c++

--- dynamic_tutorials/scripts/client.py
+++ dynamic_tutorials/scripts/client.py
@@ -1,24 +1,49 @@
 #!/usr/bin/env python
 
+import sys
+import numpy as np
+import cv2
 import rospy
-
 import dynamic_reconfigure.client
 
+lower = np.array([0, 0, 0])
+upper = np.array([0, 0, 0])
+
 def callback(config):
-    rospy.loginfo("Config set to {int_param}, {double_param}, {str_param}, {bool_param}, {size}".format(**config))
+    global lower
+    global upper
+    lower = np.array([config["h_lower"], config["s_lower"], config["v_lower"]])
+    upper = np.array([config["h_upper"], config["s_upper"], config["v_upper"]])
 
 if __name__ == "__main__":
     rospy.init_node("dynamic_client")
-
     client = dynamic_reconfigure.client.Client("dynamic_tutorials", timeout=30, config_callback=callback)
-
-    r = rospy.Rate(0.1)
-    x = 0
-    b = False
+    cap = cv2.VideoCapture(0)
+    threshold = 1000
+    r = rospy.Rate(50)
     while not rospy.is_shutdown():
-        x = x+1
-        if x>10:
-            x=0
-        b = not b
-        client.update_configuration({"int_param":x, "double_param":(1/(x+1)), "str_param":str(rospy.get_rostime()), "bool_param":b, "size":1})
+        _, frame = cap.read()
+
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        g_blur = cv2.GaussianBlur(hsv, (5, 5), 0)
+        mask = cv2.inRange(g_blur, lower, upper)
+        _, contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        if len(contours):
+            c = max(contours, key = cv2.contourArea)
+            area = cv2.contourArea(c)
+            if (area > threshold):
+                x,y,w,h = cv2.boundingRect(c)
+                cv2.drawContours(frame, c, -1, (0, 255, 0), 3)
+                print(area)
+
+        cv2.imshow("frame", frame)
+        cv2.imshow("mask", mask)
+
+        k = cv2.waitKey(5) & 0xFF
+        if k == 27:
+            break
         r.sleep()
+
+    cv2.destroyAllWindows()
+    cap.release()
---
+++ dynamic_tutorials/src/colour_detection.cpp
@@ -0,0 +1,96 @@
+#include <ros/ros.h>
+#include <vector>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <dynamic_reconfigure/server.h>
+#include <dynamic_tutorials/TutorialsConfig.h>
+
+cv::Scalar lower = cv::Scalar(0, 0, 0);
+cv::Scalar upper = cv::Scalar(180, 255, 255);
+
+void callback(dynamic_tutorials::TutorialsConfig &config, uint32_t level) {
+    lower = cv::Scalar(config.h_lower, config.s_lower, config.v_lower);
+    upper = cv::Scalar(config.h_upper, config.s_upper, config.v_upper);
+}
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "dynamic_tutorials");
+    ROS_INFO("Camera is originally set to detect dark blue!");
+    ROS_INFO("Please run the following command to launch GUI: rosrun rqt_gui rqt_gui -s reconfigure");
+
+    dynamic_reconfigure::Server<dynamic_tutorials::TutorialsConfig> server;
+    dynamic_reconfigure::Server<dynamic_tutorials::TutorialsConfig>::CallbackType f;
+    f = boost::bind(&callback, _1, _2);
+    server.setCallback(f);
+
+    cv::VideoCapture cap(0);
+    if (!cap.isOpened()) {
+        return -1;
+    }
+
+    long area_threshold = 1000;
+    cv::Mat frame, hsv, blur, mask;
+
+    ros::Rate loop_rate(50);
+
+    while (ros::ok()) {
+
+        // for debugging
+        // std::cout << lower << std::endl;
+        // std::cout << upper << std::endl;
+
+        // read the video frame
+        cap >> frame;
+
+        // convert the frame from bgr to hsv
+        cv::cvtColor(frame, hsv, cv::COLOR_BGR2HSV);
+
+        // blur tiny pixels to the surrounding pixels
+        cv::GaussianBlur(hsv, blur, cv::Size(5, 5), 0, 0);
+
+        // get pixels within pixel ranges
+        cv::inRange(blur, lower, upper, mask);
+
+        // find the largest contour
+        std::vector<std::vector<cv::Point>> contours;
+        std::vector<cv::Vec4i> hierarchy;
+        cv::findContours(mask, contours, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
+        long largest_area = 0;
+        long largest_contour_index = 0;
+
+        // check for existing of contors before finding the largest
+        if (contours.size()) {
+
+            // get the largest contour based on area
+            for(long i = 0; i< contours.size(); i++) {
+                long area = cv::contourArea(contours[i], false);  
+                if (area > largest_area) {
+                    largest_area = area;
+                    largest_contour_index = i;      
+                }
+            }
+
+            // draw the largest contour if the contour area is greater than threshold
+            if (largest_area > area_threshold) {
+                cv::Scalar color = cv::Scalar(0, 255, 0);
+                cv::drawContours(frame, contours, largest_contour_index, color, 1, 8, std::vector<cv::Vec4i>(), 0, cv::Point());
+            }
+        }
+
+        // draw the frame with the contour and the mask
+        cv::imshow("frame", frame);
+        cv::imshow("mask", mask);
+
+        // exit when ESC is pressed
+        if (cv::waitKey(30) >= 0) {
+            break;
+        }
+
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+
+
+    return 0;
+}
+
