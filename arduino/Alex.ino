081d9d9dbb6da4dec086a6d4c92138dcb6a39a38
Lim Chek Jun <chekjun@ymail.com>
2019-04-03 10:36:05 +0800

    Integrated ultrasonic function (loop / polling)
    Not tested yet.

--- arduino/Alex.ino
+++ arduino/Alex.ino
@@ -42,6 +42,9 @@ volatile unsigned long deltaDist;
 volatile unsigned long deltaTicks,targetTicks;
 volatile unsigned long newDist;
 
+// Variables for ultrasonic sensor
+long frontDuration, frontDistance, backDuration, backDistance;
+
 TResult readPacket(TPacket *packet)
 {
     char buffer[PACKET_SIZE];
@@ -462,9 +465,7 @@ void waitForHello()
 }
 
 void setup() {
-  cli();setupEINT();setupSerial();startSerial();setupMotors();startMotors();enablePullups();initializeState();sei();
-
-  
+  cli();setupEINT();setupSerial();startSerial();setupMotors();startMotors();enablePullups();initializeState();sei();setupUltrasonic();
 }
 
 void handlePacket(TPacket *packet)
@@ -487,6 +488,39 @@ void handlePacket(TPacket *packet)
     case PACKET_TYPE_HELLO:
       break;
   }
+}
+
+void setupUltrasonic() {
+  DDRD |= B10000000; // DECLARE PIN 7 OUTPUT
+  DDRB |= B010000; // DECLARE PIN 12 OUTPUT
+}
+
+void checkDistance() {
+  PORTD &= B01111111; // SET PIN 7 LOW (LEFT TRIGGER)
+  delayMicroseconds(5);
+  PORTD |= B10000000; // SET PIN 7 HIGH (LEFT TRIGGER)
+  delayMicroseconds(10);
+  PORTD &= B01111111; // SET PIN 7 LOW (LEFT TRIGGER)
+  DDRB &= B111110; // DECLARE PIN 8 INPUT (LEFT ECHO)
+  frontDuration = pulseIn(8, HIGH);
+  frontDistance = (frontDuration * 0.0343) / 2;
+  Serial.print("Distance from front wall: ");
+  Serial.print(frontDistance);
+  Serial.println("cm.");
+
+  delay(60); // wait for first ping over
+  
+  PORTB &= B101111; // SET PIN 12 TO LOW (RIGHT TRIGGER)
+  delayMicroseconds(5);
+  PORTB |= B010000; // SET PIN 12 TO HIGH (RIGHT TRIGGER)
+  delayMicroseconds(10);
+  PORTB &= B101111; // SET PIN 12 TO LOW (RIGHT TRIGGER)
+  DDRB &= B011111; // DECLARE PIN 13 AS INPUT RIGHT ECHO
+  backDuration = pulseIn(13, HIGH);
+  backDistance = (backDuration * 0.0343) / 2;
+  Serial.print("Distance from back wall: ");
+  Serial.print(backDistance);
+  Serial.println("cm.");
 }
 
 
@@ -547,5 +581,7 @@ void loop() {
     }
 
   }
+
+  checkDistance();
   
 }
